2a2f7d6c8df8af58b54a66b00fbb161dc4140b49
IrishBruse <Econn50@outlook.com>
2023-02-23 14:32:11 +0000

    Improve the Metronome Movement Closes #3

--- Pendulum/Assets/_Scripts/FollowPath.cs
+++ Pendulum/Assets/_Scripts/FollowPath.cs
@@ -14,17 +14,20 @@ public class FollowPath : MonoBehaviour
 
     private float timer = 0;
 
-    private void Start()
-    {
-    }
-
     private void Update()
     {
+        // Update the timer for lerping at the given frequency
         timer += Time.deltaTime * frequency;
 
+        // PingPong will return a value between 0 and 1
         float t = Mathf.PingPong(timer, 1f);
 
+        // Lerp between the start and end positions
         ball.position = Vector3.Lerp(start.position, end.position, t);
-        ball.position += new Vector3(0, curve.Evaluate(t) * 0.8f, 0);
+
+        const float curveHeight = 0.8f;
+
+        // Move the ball up and down based on the curve
+        ball.position += new Vector3(0, curve.Evaluate(t) * curveHeight, 0);
     }
 }
